ae42d5b92bcfd0f4e567b47c2faffd2d535ef245
Leonardo Cordeiro <leonardo.cordeiro@caelum.com.br>
2016-06-29 19:24:23 -0300

    inicio da configuracao de oae oauth2 (authorization server)

---
+++ rolebackend/src/main/java/br/com/mytho/role/infra/security/configuration/SecurityBootstrap.java
@@ -0,0 +1,7 @@
+package br.com.mytho.role.infra.security.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityBootstrap extends AbstractSecurityWebApplicationInitializer {
+
+}
