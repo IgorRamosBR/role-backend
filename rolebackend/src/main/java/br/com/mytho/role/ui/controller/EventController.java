d52606efdb6e418483e2350e318fc2ec70821d03
LucasGarcez <lucasgarcezdev@gmail.com>
2016-07-07 09:30:14 -0300

    refactoring findBy (EventController)

--- rolebackend/src/main/java/br/com/mytho/role/ui/controller/EventController.java
+++ rolebackend/src/main/java/br/com/mytho/role/ui/controller/EventController.java
@@ -7,10 +7,10 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.mytho.role.domain.model.Event;
@@ -42,9 +42,9 @@ public class EventController {
 	}
 	
 
-	@RequestMapping(value="evento",method=RequestMethod.GET, 
+	@RequestMapping(value="/{id}",method=RequestMethod.GET, 
 		    produces=MediaType.APPLICATION_JSON_VALUE)
-	public Event findBy(@RequestParam Long id){
+	public Event findBy(@PathVariable Long id){
 		 return events.findBy(id);
 	}
 }
