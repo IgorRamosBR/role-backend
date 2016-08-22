d25110315f3b2d8e73410ec5a7aa1fe39a69ebeb
LucasGarcez <lucasgarcezdev@gmail.com>
2016-08-22 19:09:23 -0300

    refatorando Oauth2Test

--- rolebackend/src/test/java/br/com/mytho/role/oauth/Oauth2Test.java
+++ rolebackend/src/test/java/br/com/mytho/role/oauth/Oauth2Test.java
@@ -1,17 +1,17 @@
 package br.com.mytho.role.oauth;
 
 import static io.restassured.RestAssured.given;
-//import static io.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Test;
+//import static io.restassured.matcher.RestAssuredMatchers.*;
 
 public class Oauth2Test {
 	
 	@Test
 	public void basicAuthentication(){
 		given().
-		param("scoped", "public-area").
+		param("scope", "public-area").
+		param("grant_type", "client_credentials").
 		auth().
 		preemptive().
 		basic("mobile-client", "08282424-432a-11e6-beb8-9e71128cae77").
@@ -20,31 +20,13 @@ public class Oauth2Test {
 		then().statusCode(200);
 	}
 	
-	@Test
-	public void authenticationWithMethodGet(){
-		//Method get não é permitido, portando deve retornar 405
-		given().
-		param("grant_type", "password").
-		param("username", "login").
-		param("password", "123").
-		param("scoped", "public-area").
-		auth().
-		preemptive().
-		basic("mobile-client", "08282424-432a-11e6-beb8-9e71128cae77").
-		
-	when().
-		get("http://localhost:8080/rolebackend/oauth/token").
-	then().statusCode(405);
-	}
-	
+
 	@Test
 	public void authenticationWithMethodPost(){
 		//autenticação básica através do método post
 		given().
-			param("grant_type", "password").
-			param("username", "login").
-			param("password", "123").
-			param("scoped", "public-area").
+			param("scope", "public-area").
+			param("grant_type", "client_credentials").
 			auth().
 			preemptive().
 			basic("mobile-client", "08282424-432a-11e6-beb8-9e71128cae77").
@@ -56,12 +38,43 @@ public class Oauth2Test {
 	}
 	
 	@Test
-	public void authenticationWithBadUsername(){
+	public void authenticationWithMethodGet(){
+		//Method get não é permitido, portando deve retornar 405
+		given().
+		param("scope", "public-area").
+		param("grant_type", "client_credentials").
+		auth().
+		preemptive().
+		basic("mobile-client", "08282424-432a-11e6-beb8-9e71128cae77").
+		
+	when().
+		get("http://localhost:8080/rolebackend/oauth/token").
+	then().statusCode(405);
+	}
+	
+	@Test
+	public void basicAuthenticationWithPassword(){
 		given().
 		param("grant_type", "password").
-		param("username", "badusername").
+		param("username", "login").
 		param("password", "123").
-		param("scoped", "public-area").
+		auth().
+		preemptive().
+		basic("mobile-client", "08282424-432a-11e6-beb8-9e71128cae77").
+		
+	when().
+		post("http://localhost:8080/rolebackend/oauth/token").
+	then().statusCode(200);
+	}
+	
+	
+	@Test
+	public void authenticationWithBadUsername(){
+		given().
+		param("scope", "public-area").
+		param("grant_type", "password").
+		param("username", "badUsername").
+		param("password", "123").
 		auth().
 		preemptive().
 		basic("mobile-client", "08282424-432a-11e6-beb8-9e71128cae77").
@@ -73,8 +86,40 @@ public class Oauth2Test {
 	}
 	
 	@Test
-	public void authenticationWithoutScoped(){
+	public void authenticationWithBadPassword(){
 		given().
+		param("scope", "public-area").
+		param("grant_type", "password").
+		param("username", "login").
+		param("password", "badPassword").
+		auth().
+		preemptive().
+		basic("mobile-client", "08282424-432a-11e6-beb8-9e71128cae77").
+		
+	when().
+		post("http://localhost:8080/rolebackend/oauth/token").
+	then().statusCode(401);
+		
+	}
+	
+	@Test
+	public void authenticationWithClientCredentialsAndWithoutScoped(){
+		given().
+		param("grant_type", "client_credentials").
+		auth().
+		preemptive().
+		basic("mobile-client", "08282424-432a-11e6-beb8-9e71128cae77").
+		
+	when().
+		post("http://localhost:8080/rolebackend/oauth/token").
+	then().statusCode(400);
+		
+	}
+	
+	@Test
+	public void authenticationWithPrivateScope(){
+		given().
+		param("scope", "private-area").
 		param("grant_type", "password").
 		param("username", "login").
 		param("password", "123").
@@ -84,8 +129,8 @@ public class Oauth2Test {
 		
 	when().
 		post("http://localhost:8080/rolebackend/oauth/token").
-	then().statusCode(400);
-		
+	then().statusCode(200);
+	
 	}
 
 }
